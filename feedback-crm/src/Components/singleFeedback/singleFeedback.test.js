1889c109b4ad541fe34c545c2f78e2c8a5a508f9
“justintangjx” <just.think.tang@gmail.com>
2018-03-28 17:27:00 +0800

    set up enzyme testing structure

---
+++ feedback-crm/src/Components/singleFeedback/singleFeedback.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from "enzyme";
+import DisplaySingleFeedback from './singleFeedback';
+
+describe("singleFeedback", () => {
+    it('should display action and improvement', () => {
+        const 
+    })
+})
