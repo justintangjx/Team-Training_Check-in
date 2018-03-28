c57b1f2588eb07b71e4504ae7a5ff8c3141a255a
“justintangjx” <just.think.tang@gmail.com>
2018-03-28 16:32:20 +0800

    card display

--- feedback-crm/src/App.js
+++ feedback-crm/src/App.js
@@ -9,6 +9,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a href="/" className="nav-link active">What was AWESOME</a>
+        </li>
+        <li className="nav-item">
+        <a href="/" className="nav-link dropdown-toggle" data-toggle="dropdown">What to work on</a>
+        </li>
+        </ul>
+        <p />
         <FeedbackBox />
         
       </div>
--- feedback-crm/src/Components/singleFeedback/singleFeedback.js
+++ feedback-crm/src/Components/singleFeedback/singleFeedback.js
@@ -3,18 +3,16 @@ import React, { Component } from "react";
 class DisplaySingleFeedback extends Component {
   render() {
     return (
-      <div className="feedback">
-        <div className="card" style={{ width: "25rem", textAlign: "center" }}>
-          <div className="card-body">
-            <p className="feedback-header">{this.props.feedback.action}</p>
-            <p className="feedback-body">{this.props.feedback.improvement}</p>
-            <button
-              className="btn btn-secondary btn-sm"
-              onClick={this.props.handleDelete}
-            >
-              Delete
-            </button>
-          </div>
+      <div className="card" style={{ width: '20rem', display:"inline-block"}} >
+        <div className="card-body">
+          <p className="feedback-header">{this.props.feedback.action}</p>
+          <p className="feedback-body">{this.props.feedback.improvement}</p>
+          <button
+            className="btn btn-secondary btn-sm"
+            onClick={this.props.handleDelete}
+          >
+            Delete
+          </button>
         </div>
       </div>
     );
