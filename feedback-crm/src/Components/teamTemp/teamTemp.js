09e2b882d128fb960b974b9422dd4362d287c5e0
“justintangjx” <just.think.tang@gmail.com>
2018-04-01 17:11:24 +0800

    setting up dynamic data

--- feedback-crm/src/Components/teamTemp/teamTemp.js
+++ feedback-crm/src/Components/teamTemp/teamTemp.js
@@ -1,27 +1,37 @@
-import React from 'react';
-import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from "recharts";
+import React from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
 
 const teamData = [
-    {name: 'Austin', "How was this week on a scale of 1 to 10": 2},
-    {name: 'Bustin', "How was this week on a scale of 1 to 10": 5},
-    {name: 'Custin', "How was this week on a scale of 1 to 10": 8},
-    {name: 'Dustin', "How was this week on a scale of 1 to 10": 9},
-    {name: 'Eustin', "How was this week on a scale of 1 to 10": 7},
+  { name: "Austin", "How was this week on a scale of 1 to 10": 2 },
+  { name: "Bustin", "How was this week on a scale of 1 to 10": 5 },
+  { name: "Custin", "How was this week on a scale of 1 to 10": 8 },
+  { name: "Dustin", "How was this week on a scale of 1 to 10": 9 },
+  { name: "Eustin", "How was this week on a scale of 1 to 10": 7 }
 ];
 
 const teamBarChart = () => (
-    <div id="simple-bar-chart">
-        <BarChart width={500} height={300} data={teamData}
-            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-       <XAxis dataKey="name"/>
-       <YAxis ticks={[2, 4, 6, 8, 10]} type="number" />
-       <CartesianGrid strokeDasharray="3 3"/>
-       <Tooltip/>
-       <Legend />
-       <Bar dataKey="How was this week on a scale of 1 to 10" fill="#8884d8" />
-       
-      </BarChart>
-    </div>
+  <div id="simple-bar-chart">
+    <BarChart
+      width={500}
+      height={300}
+      data={teamData}
+      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+    >
+      <XAxis dataKey="name" />
+      <YAxis ticks={[2, 4, 6, 8, 10]} type="number" />
+      <CartesianGrid strokeDasharray="3 3" />
+      <Legend />
+      <Bar dataKey="How was this week on a scale of 1 to 10" fill="#8884d8" />
+    </BarChart>
+  </div>
 );
 
 export default teamBarChart;
---
+++ feedback-crm/src/Components/teamTemp/teamTempForm.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class TeamScoreForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      weeklyScore: ""
+    };
+    this.handleChangeForNameInput = this.handleChangeForNameInput.bind(this);
+    this.handleChangeForWeeklyScoreInput = this.handleChangeForWeeklyScoreInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangeForWeeklyScoreInput(event) {
+      this.setState({weeklyScore: event.target.value});
+  }
+  handleSubmit(event) {
+      event.preventDefault();
+      this.props.
+  }
+}
