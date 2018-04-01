3f75a04629c18d46f3ca4d366c086b2c2811e46e
“justintangjx” <just.think.tang@gmail.com>
2018-04-01 16:15:49 +0800

    added team bar chart

--- feedback-crm/src/App.js
+++ feedback-crm/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import FeedbackBox from "./Components/feedbackBox/feedbackBox";
 import { BrowserRouter, Route } from "react-router-dom";
 import Navtabs from './Components/navtabs';
+import teamBarChart from './Components/teamTemp/teamTemp';
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
           <Navtabs />
           <Route exact path="/awesome" component={FeedbackBox} />
           <Route exact path="/toimprove" component ={FeedbackBox} />
+          <Route exact path="/manager" component = {teamBarChart} />
           <br />
         </div>
 
