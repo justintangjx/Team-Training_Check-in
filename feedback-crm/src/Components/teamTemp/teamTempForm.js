acb8cc26f5eeff9efafec37834896b52d54d67c8
“justintangjx” <just.think.tang@gmail.com>
2018-04-04 23:37:53 +0800

    draft

--- feedback-crm/src/Components/teamTemp/teamTempForm.js
+++ feedback-crm/src/Components/teamTemp/teamTempForm.js
@@ -1,24 +1,37 @@
-// import React, { Component } from "react";
+import React, { Component } from "react";
 
-// class TeamScoreForm extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       name: "",
-//       weeklyScore: ""
-//     };
-//     this.handleChangeForNameInput = this.handleChangeForNameInput.bind(this);
-//     this.handleChangeForWeeklyScoreInput = this.handleChangeForWeeklyScoreInput.bind(this);
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//   }
+class TeamScoreForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      weeklyScore: ""
+    };
+    this.handleChangeForNameInput = this.handleChangeForNameInput.bind(this);
+    this.handleChangeForWeeklyScoreInput = this.handleChangeForWeeklyScoreInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-//   handleChangeForWeeklyScoreInput(event) {
-//       this.setState({weeklyScore: event.target.value});
-//   }
-//   handleSubmit(event) {
-//       event.preventDefault();
-//       this.addFeedback(this.state.name, this.state.weeklyScore)
-//   }
+  handleChangeForWeeklyScoreInput(event) {
+      this.setState({weeklyScore: event.target.value});
+  }
 
-//   addFeedback()
-// }
+  handleChangeForNameInput(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  handleSubmit(event) {
+      event.preventDefault();
+      this.addFeedback(this.state.name, this.state.weeklyScore)
+  }
+
+  addFeedback()
+}
+
+render() {
+    const{ name, weeklyScore } = this.state;
+    const checkForTwoEmptyFields = name === "" || weeklyScore === "";
+    return (
+        
+    )
+}
