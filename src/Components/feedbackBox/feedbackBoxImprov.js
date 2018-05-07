b1ce626b206f9b70deac1fc8bdd351b92bae9a88
“justintangjx” <just.think.tang@gmail.com>
2018-05-07 21:01:19 +0800

    added break in improv component

--- src/Components/feedbackBox/feedbackBoxImprov.js
+++ src/Components/feedbackBox/feedbackBoxImprov.js
@@ -31,6 +31,7 @@ class FeedbackBoxImprov extends Component {
   render() {
     return (
       <div id="feedbackHistory">
+      <br />
         <FeedbackFormImprov
           saveFeedback={this.saveFeedback.bind(this)}
           newFeedback={this.state.newFeedback}
--- src/utils/seedData.js
+++ src/utils/seedData.js
@@ -1,4 +1,4 @@
 export const feedbackList = [
-    {action: "interupted while classmates were talking", improvement: "wait for classmates to finish before airing views"},
+    {action: "Improving churn rate", improvement: "Difficult to pull data from marketing"},
     
 ];
